04ef3c98664a5d1a91666cb86e5704c16c0373c4
Gokhan Kurt <krtgkn@gmail.com>
2021-05-03 16:05:04 +0300

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import * as gtag from 'lib/gtag';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import 'styles/global.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Component {...pageProps} />
-  </>;
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => gtag.pageview(url);
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
 }
