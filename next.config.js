0b6374c931409923032481b80001bee3b90e829e
Gokhan Kurt <krtgkn@gmail.com>
2021-07-11 23:45:45 +0300

    enable debugging

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   webpack5: true,
+  webpack: (config) => {
+    config.output = config.output || {};
+    config.output.devtoolModuleFilenameTemplate = function (info) {
+      return "file:///" + encodeURI(info.absoluteResourcePath);
+    }
+    return config;
+  },
 }
